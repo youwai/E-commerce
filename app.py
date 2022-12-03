7d4b13155bc012eca15f90741d235c5c655b5ad7
You Wai <lyw5823@gmail.com>
2022-12-03 19:09:07 +0800

    completed purchase history

--- app.py
+++ app.py
@@ -2,17 +2,29 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+user = ''
+
 @app.route('/')
 def index():
     return redirect(url_for('login'))
 
 @app.route('/login')
 def login():
+    user = ''
     return render_template('login.html')
 
 @app.route('/home', methods = ['POST'])
 def home():
     if request.method == 'POST':
-        member_id = request.form['member_id']
+        global user 
+        user = request.form['member_id']
 
-        return render_template('main.html', member_id = member_id)
+        return redirect(url_for('main'))
+
+@app.route('/main')
+def main():
+    return render_template('main.html', user = user)
+
+@app.route('/history')
+def history():
+    return render_template('history.html', user = user)
