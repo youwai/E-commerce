825ab7204625a9fc3886d6c7c2c570169578b858
You Wai <lyw5823@gmail.com>
2022-12-03 18:23:44 +0800

    completed html linkages

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+@app.route('/login')
 def login():
-    return render_template('main.html')
+    return render_template('login.html')
+
+@app.route('/home', methods = ['POST'])
+def home():
+    if request.method == 'POST':
+        member_id = request.form['member_id']
+
+        return render_template('main.html', member_id = member_id)
